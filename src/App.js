3b25b7fde8ad32e7dffdf8a118ea1f5de47b615a
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-03 12:38:29 -0600

    Added header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Table from "./components/table";
 function App() {
     return (
         <div className='container'>
+            <h1 className="center-align">Hockey Stats Tracker</h1>
               <Table />
         </div>
     )
