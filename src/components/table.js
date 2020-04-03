e4dfdb307f936c4eae7f10c46e41a50f7b4142fb
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-03 12:21:34 -0600

    Removed state logic

--- src/components/table.js
+++ src/components/table.js
@@ -23,43 +23,43 @@ export default function MaterialTableDemo() {
       title="Player Stats"
       columns={state.columns}
       data={state.data}
-      editable={{
-        onRowAdd: (newData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState((prevState) => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: (oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-      }}
+      // editable={{
+      //   onRowAdd: (newData) =>
+      //     new Promise((resolve) => {
+      //       setTimeout(() => {
+      //         resolve();
+      //         setState((prevState) => {
+      //           const data = [...prevState.data];
+      //           data.push(newData);
+      //           return { ...prevState, data };
+      //         });
+      //       }, 600);
+      //     }),
+      //   onRowUpdate: (newData, oldData) =>
+      //     new Promise((resolve) => {
+      //       setTimeout(() => {
+      //         resolve();
+      //         if (oldData) {
+      //           setState((prevState) => {
+      //             const data = [...prevState.data];
+      //             data[data.indexOf(oldData)] = newData;
+      //             return { ...prevState, data };
+      //           });
+      //         }
+      //       }, 600);
+      //     }),
+      //   onRowDelete: (oldData) =>
+      //     new Promise((resolve) => {
+      //       setTimeout(() => {
+      //         resolve();
+      //         setState((prevState) => {
+      //           const data = [...prevState.data];
+      //           data.splice(data.indexOf(oldData), 1);
+      //           return { ...prevState, data };
+      //         });
+      //       }, 600);
+      //     }),
+      // }}
     />
   );
 }
