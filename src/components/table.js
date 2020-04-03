649fee02f9ba441ac39a9e8f3df3b8a6dd219642
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-03 12:17:24 -0600

    Switched to material-ui and material-table

--- src/components/table.js
+++ src/components/table.js
@@ -1,29 +1,65 @@
-import React from "react";
-import TableRow from "./tableRow"
-import Stats from "./stats.json"
+import React from 'react';
+import MaterialTable from 'material-table';
+import Stats from './stats.json';
 
-function Table() {
-    return (
-        <table className='highlight responsive-table'>
-            <thead>
-                <tr>
-                    <th>Player</th>
-                    <th>Team</th>
-                    <th>S/C</th>
-                    <th>Pos</th>
-                    <th>GP</th>
-                    <th>G</th>
-                    <th>A</th>
-                    <th>P</th>
-                </tr>
-            </thead>
-            <tbody>
-                {Stats.map((obj) =>
-                    <TableRow key={obj.id} {...obj} />
-                )}
-            </tbody>
-        </table>
-    )
+export default function MaterialTableDemo() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'Player', field: 'player' },
+      { title: 'Team', field: 'team' },
+      { title: 'Position', field: 'position', lookup: { L: 'Left Wing', R: 'Right Wing', C: 'Center', D: 'Defense', G: 'Goalie'}},
+      { title: 'Shoot/Catch', field: 'sc', lookup: { L: 'Left', R: 'Right'} },
+      { title: 'Games Played', field: 'played', type: 'numeric' },
+      { title: 'Goals', field: 'goals', type: 'numeric' },
+      { title: 'Assists', field: 'assists', type: 'numeric' },
+      { title: 'Points', field: 'points', type: 'numeric' },
+    ],
+    data: Stats
+
+  });
+
+  return (
+    <MaterialTable
+      title="Player Stats"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: (newData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              setState((prevState) => {
+                const data = [...prevState.data];
+                data.push(newData);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              if (oldData) {
+                setState((prevState) => {
+                  const data = [...prevState.data];
+                  data[data.indexOf(oldData)] = newData;
+                  return { ...prevState, data };
+                });
+              }
+            }, 600);
+          }),
+        onRowDelete: (oldData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              setState((prevState) => {
+                const data = [...prevState.data];
+                data.splice(data.indexOf(oldData), 1);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+      }}
+    />
+  );
 }
-
-export default Table;
