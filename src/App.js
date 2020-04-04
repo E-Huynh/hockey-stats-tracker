9aa1dc4b405daa1cb23992065ca31ee0cf3bbf32
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-04 08:18:54 -0600

    set up colors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Teams from "./components/teams";
 function App() {
     return (
         <div className='container'>
-            <h1 className="center-align">Hockey Stats Tracker</h1>
+            <h1 className="center-align white-text">Hockey Stats Tracker</h1>
               <Teams />
         </div>
     )
