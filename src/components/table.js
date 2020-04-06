65e018993c2f5c34b30c15884beee0295f195180
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-06 17:40:57 -0600

    Removed state from Table

--- src/components/table.js
+++ src/components/table.js
@@ -6,59 +6,59 @@ export default function Table(props) {
     columns: [
       { title: 'Player', field: 'player' },
       { title: 'Team', field: 'team' },
-      { title: 'Position', field: 'position', lookup: { L: 'Left Wing', R: 'Right Wing', C: 'Center', D: 'Defense', G: 'Goalie'}},
-      { title: 'Shoot/Catch', field: 'sc', lookup: { L: 'Left', R: 'Right'} },
+      { title: 'Position', field: 'position', lookup: { L: 'Left Wing', R: 'Right Wing', C: 'Center', D: 'Defense', G: 'Goalie' } },
+      { title: 'Shoot/Catch', field: 'sc', lookup: { L: 'Left', R: 'Right' } },
       { title: 'Games Played', field: 'played', type: 'numeric' },
       { title: 'Goals', field: 'goals', type: 'numeric' },
       { title: 'Assists', field: 'assists', type: 'numeric' },
       { title: 'Points', field: 'points', type: 'numeric' },
     ],
-    data: props.stats
   });
 
+  console.log('props.stats: ', props.stats)
   return (
     <MaterialTable
       title="Player Stats"
       columns={state.columns}
-      data={state.data}
-      options={{paging: false}}
-      // editable={{
-      //   onRowAdd: (newData) =>
-      //     new Promise((resolve) => {
-      //       setTimeout(() => {
-      //         resolve();
-      //         setState((prevState) => {
-      //           const data = [...prevState.data];
-      //           data.push(newData);
-      //           return { ...prevState, data };
-      //         });
-      //       }, 600);
-      //     }),
-      //   onRowUpdate: (newData, oldData) =>
-      //     new Promise((resolve) => {
-      //       setTimeout(() => {
-      //         resolve();
-      //         if (oldData) {
-      //           setState((prevState) => {
-      //             const data = [...prevState.data];
-      //             data[data.indexOf(oldData)] = newData;
-      //             return { ...prevState, data };
-      //           });
-      //         }
-      //       }, 600);
-      //     }),
-      //   onRowDelete: (oldData) =>
-      //     new Promise((resolve) => {
-      //       setTimeout(() => {
-      //         resolve();
-      //         setState((prevState) => {
-      //           const data = [...prevState.data];
-      //           data.splice(data.indexOf(oldData), 1);
-      //           return { ...prevState, data };
-      //         });
-      //       }, 600);
-      //     }),
-      // }}
+      data={props.stats}
+      options={{ paging: false }}
+    // editable={{
+    //   onRowAdd: (newData) =>
+    //     new Promise((resolve) => {
+    //       setTimeout(() => {
+    //         resolve();
+    //         setState((prevState) => {
+    //           const data = [...prevState.data];
+    //           data.push(newData);
+    //           return { ...prevState, data };
+    //         });
+    //       }, 600);
+    //     }),
+    //   onRowUpdate: (newData, oldData) =>
+    //     new Promise((resolve) => {
+    //       setTimeout(() => {
+    //         resolve();
+    //         if (oldData) {
+    //           setState((prevState) => {
+    //             const data = [...prevState.data];
+    //             data[data.indexOf(oldData)] = newData;
+    //             return { ...prevState, data };
+    //           });
+    //         }
+    //       }, 600);
+    //     }),
+    //   onRowDelete: (oldData) =>
+    //     new Promise((resolve) => {
+    //       setTimeout(() => {
+    //         resolve();
+    //         setState((prevState) => {
+    //           const data = [...prevState.data];
+    //           data.splice(data.indexOf(oldData), 1);
+    //           return { ...prevState, data };
+    //         });
+    //       }, 600);
+    //     }),
+    // }}
     />
   );
 }
--- src/components/teams.js
+++ src/components/teams.js
@@ -12,24 +12,27 @@ const stats = Stats
 class Teams extends React.Component {
     // states
     state = {
-        team: "",
-        stats: stats
+        team: ""
     }
 
     // methods
     displayRandomTeam = event => {
         event.preventDefault()
-        const rand = Math.floor(Math.random() * 31)
+        const rand = Math.floor(Math.random() * teamArr.length)
         this.setState({team: teamArr[rand]})
     };
 
     // render
     render() {
+        const str = this.state.team.replace(/\s/g, '')
+        console.log('str', str)
+        const data = Stats[str]
+        console.log('data' ,data)
         return (
             <div className='center-align mainDisplay'>
                 <TeamName team={this.state.team !== "" ? this.state.team : "Generate Random Team"}/>
                 <RandomBtn displayRandomTeam={this.displayRandomTeam}/>
-                <Table stats={this.state.stats}/>
+                <Table stats={data}/>
             </div>
         );
     }
