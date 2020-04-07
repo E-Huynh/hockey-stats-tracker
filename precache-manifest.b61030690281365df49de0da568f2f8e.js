18addf9fd055af3315e68774b3479c6251486d1f
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-04-07 17:58:10 -0600

    Updates

---
+++ precache-manifest.b61030690281365df49de0da568f2f8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b07f2b5a49700edcb6cc1016abfc900",
+    "url": "/hockey-stats-tracker/index.html"
+  },
+  {
+    "revision": "46eed66e858611cb2f0d",
+    "url": "/hockey-stats-tracker/static/js/2.602968d8.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/hockey-stats-tracker/static/js/2.602968d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c979d64b9fe5b274d8",
+    "url": "/hockey-stats-tracker/static/js/main.d2e90bef.chunk.js"
+  },
+  {
+    "revision": "3fd245af5c197533c8e2",
+    "url": "/hockey-stats-tracker/static/js/runtime-main.14804fc7.js"
+  }
+]);
